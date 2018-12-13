91b4f9eb201410ad1f558ec2d7f3aabc353f10a8
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-12-13 20:03:07 +0800

    add format

---
+++ format.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -d ~/tools ]; then
+    echo "create tools folder under $HOME"
+    cd ~ && mkdir tools
+fi
+
+if [ ! -d ~/tools/spacecommander ]; then
+    echo "download spacecommander to $HOME/tools"
+    git clone git@github.com:square/spacecommander.git    
+fi
+
+if [ `grep -c "SPACECOMMANDER" ~/.zshrc` -eq '0' ]; then
+    echo "add to .zshrc"
+	echo "export SPACECOMMANDER=~/tools/spacecommander" >> ~/.zshrc
+fi
+
+source ~/.zshrc
+
+$SPACECOMMANDER/setup-repo.sh
+
